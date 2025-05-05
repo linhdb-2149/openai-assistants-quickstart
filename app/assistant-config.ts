374ade942c1fbb4a6f09836a841aeccddb08003c
linhdb-2149 <61034772+linhdb-2149@users.noreply.github.com>
2025-05-05 11:14:46 +0700

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_iiSL9xKkNfkxHkD30t7EJLVG"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
